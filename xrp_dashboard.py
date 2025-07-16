d3461af9cdad586466adb6cae5d4760572b4f3bd
DI-main-ai <daanesh.ibrahim@gmail.com>
2025-07-16 13:09:16 -0500

    Update xrp_dashboard.py

--- xrp_dashboard.py
+++ xrp_dashboard.py
@@ -507,7 +507,7 @@ with tab2:
             margin=dict(l=120, r=60, t=80, b=60),
             bargap=0.50,     # Use whatever gap looks best
             dragmode=False,
-            uniformtext_minsize=15,    # <<--- Force label size
+            uniformtext_minsize=10,    # <<--- Force label size
             uniformtext_mode='show',
             #width=950,                 # (Optional, remove if too wide for your layout)
         )
