ea2f9acc387210503b2a92bdff6668a7d3135e93
DI-main-ai <daanesh.ibrahim@gmail.com>
2025-07-23 14:25:34 -0500

    Update xrp_dashboard.py

--- xrp_dashboard.py
+++ xrp_dashboard.py
@@ -616,7 +616,7 @@ with tab2:
                 fixedrange=True,
                 categoryorder='array',
                 categoryarray=fixed_order[::-1],
-                tickfont=dict(size=14),
+                tickfont=dict(size=16),
             ),
         )
     
